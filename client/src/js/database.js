f05324eb7497e29e7ee130f9446aa16b2b16a698
Tyler Esselman <tme91302@gmail.com>
2022-10-28 17:58:13 -0700

    filled out database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -8,13 +8,13 @@ const initdb = async () =>
         return;
       }
       db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      console.log('jate database created, well done.');
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.log('Post to the database');
+  console.log('PUTtin some stuff in the database');
 
   const textDb = await openDB('jate', 1);
 
@@ -22,10 +22,35 @@ export const putDb = async (content) => {
 
   const store = tx.objectStore('jate');
 
-  console.error('putDb not implemented');
+  const request = store.put({ id: 1, value: content });
+
+  const result = await request;
+
+  console.log('Got that saved to the database', result)
+
+  // console.error('putDb not implemented');
 }
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('GET from the database');
+
+  // Create a connection to the database database and version we want to use.
+  const textDb = await openDB('jate', 1);
+
+  // Create a new transaction and specify the database and data privileges.
+  const tx = textDb.transaction('jate', 'readonly');
+
+  // Open up the desired object store.
+  const store = tx.objectStore('jate');
+
+  // Use the .getAll() method to get all data in the database.
+  const request = store.getAll();
+
+  // Get confirmation of the request.
+  const result = await request;
+  console.log('result.value', result);
+  return result?.value;
+};
 
 initdb();
